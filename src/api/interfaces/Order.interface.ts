33152acf7943b2462bcac53eae91bafb96f5d4c3
fatiudeen <shehufatiudeen@gmail.com>
2022-06-27 16:47:59 +0100

    fixed cart

--- src/api/interfaces/Order.interface.ts
+++ src/api/interfaces/Order.interface.ts
@@ -14,7 +14,7 @@ export type CartInterface = {
 };
 
 export interface OrderInterface extends Document {
-  cart: CartInterface[];
+  cart: any;
   totalPrice: number;
   buyer: Types.ObjectId;
   seller: Types.ObjectId;
