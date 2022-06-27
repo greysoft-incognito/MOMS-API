63f62bc3d79c57cb59bda029d919b74c7f1ab693
fatiudeen <shehufatiudeen@gmail.com>
2022-06-27 17:12:11 +0100

    fixed json

--- src/api/services/order.service.ts
+++ src/api/services/order.service.ts
@@ -8,6 +8,7 @@ export default {
     try {
       const order = new Order(data);
       const result = await order.save();
+      result.cart = JSON.parse(result.cart);
       return result;
     } catch (error: any) {
       throw new Error(error);
